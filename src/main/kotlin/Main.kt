1820f5acdaec1ab6cafa949089d665aeeac9b6f6
abdo <abdelmageed2511@gmail.com>
2022-08-18 22:14:52 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var mymap = mutableMapOf(
+        "ali" to 20,
+        "abdo" to 30,
+        "moh" to 32
+
+    )
+
+    mymap["mi"]=30 
+    println(mymap["abdo"])
+
+
+}
+
